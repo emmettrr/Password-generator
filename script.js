8adaa8f6824e479c4540298c70c1f47c765f2570
Emmett Rushin <emmettrushin@gmail.com>
2021-04-12 15:55:51 -0400

    trying to write function

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // Assignment Code
-var lowLet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var upLet = "abcdefghijklmnopqrstuvwxyz"
-var num = "0123456789"
-var spec = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+var string = prompt("Enter the number of characters for desired password length");
+
+if (string > 128) {
+  
+} else { alert("Password is too long")
+  
+}
+
+var pass = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
 
 var generateBtn = document.querySelector('#generate');
 function generatePassword() {
   return;
 }
+
 // Write password to the #password input
 function writePassword() {
   console.log('writing password');
-  var password = generatePassword();
+  var pass = generatePassword();
   var passwordText = document.querySelector('#password');
   passwordText.value = password;
 }
