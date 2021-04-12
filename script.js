c86d12f852c3d6dc382dc40fe6f62ae793ad0340
Emmett Rushin <emmettrushin@gmail.com>
2021-04-12 19:33:00 -0400

    added prompt for passwod length--fixed

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ var char = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 var string = prompt("Enter the number of characters for desired password length between 8 and 128");
 
-if (string > 8 || string > 128) {
+if (string < 8 || string > 128) {
   
 } else { prompt ("That is an invalid number. Try again!")
   
 }
+
+
 
 
 
