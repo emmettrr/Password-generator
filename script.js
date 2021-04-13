894e646a05d1f9d9a10bfa586b50e059f189b4eb
Emmett Rushin <emmettrushin@gmail.com>
2021-04-13 18:08:18 -0400

    fixed formatting and varible name

--- script.js
+++ script.js
@@ -7,58 +7,62 @@ var newPass = "";
 //Function to generate random password
 
 function generatePassword () {
-  var string = prompt("Enter the number of characters for desired password length between 8 and 128.");
+  var pass = prompt("Enter the number of characters for desired password length between 8 and 128.");
  
 
-if (string < 8 || string > 128) {
-  alert("That is an invalid number. Try again!")
-  return generatePassword ();
-}
+   if (pass < 8 || pass > 128) {
+   alert("That is an invalid number. Try again!")
+   return generatePassword ();
+   }
 
-if (string => 8 && string <= 128) {
-  low = confirm("Do you want lower case letters within your password?")
-  cap = confirm("Do you want capitalized letters in your password?")
-  num = confirm("Do you want numbers in your password?")
-  char = confirm("Do you want special characters in your password?")
-  
-  if (low === false && cap === false && num === false && char === false) {
-   alert("You need to make a selection. Try again!");
-   return generatePassword();
-}
+   if (pass => 8 && pass <= 128) {
+      low = confirm("Do you want lower case letters within your password?")
+      cap = confirm("Do you want capitalized letters in your password?")
+      num = confirm("Do you want numbers in your password?")
+      char = confirm("Do you want special characters in your password?")
+   
+      if (low === false && cap === false && num === false && char === false) {
+         alert("You need to make a selection. Try again!");
+         return generatePassword();
+      }
 
-  if (low == true) {
-     charactersL = "abcdefghijklmnopqrstuvwxyz" }
-    else {
-       charactersL = ""
-    }
-    
-    if (cap == true) {
-      charactersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+      if (low == true) {
+         charactersL = "abcdefghijklmnopqrstuvwxyz" 
+      }
+      else {
+         charactersL = ""
+      }
+      
+      if (cap == true) {
+         charactersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+      }
       else {
          charactersUp = ""
       }
 
       if (num == true) {
-         numbers = "0123456789"}
-        else {
-           numbers = ""
-        }
+         numbers = "0123456789"
+      }
+      else {
+         numbers = ""
+      }
 
-        if (char == true) {
-           specChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" }
-          else {
-             specChar = ""
-          }
-         
+      if (char == true) {
+         specChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
+      } 
+      else {
+         specChar = ""
+      }
+            
 
-    random = (charactersL + charactersUp + numbers + specChar)
+      random = (charactersL + charactersUp + numbers + specChar)
 
-    for (let i = 0, n = random.length; i < string; ++i) {
-    newPass += random.charAt(Math.floor(Math.random() * n));
-    }
+      for (let i = 0; i < pass; ++i) {
+         newPass += random.charAt(Math.floor(Math.random() * random.length));
+      }
 
-}
-return newPass;
+   }
+   return newPass;
 }
 
 // Write password to the #password input
